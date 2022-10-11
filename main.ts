168f080e97d0c0944cf7f2fd647bf09ad531650c
Stephen Koller <stephenkoller@gmail.com>
2022-10-11 12:29:46 -0400

    Lint / type fix

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 import { readFile } from "./lib/file.ts";
 
 // parse console args
-if(Deno.args.length >= 1) {
+if (Deno.args.length >= 1) {
   const arg1 = Deno.args[0];
 
   const htmlFile = await readFile(arg1);
 
   try {
-    await Deno.mkdir('./dest');
-  } catch () {
+    await Deno.mkdir("./dest");
+  } catch (_error) {
     // no-op, swallow error if directory already exists
   }
 
-  await Deno.writeTextFile('./dest/index.html', htmlFile);
+  if (htmlFile) {
+    await Deno.writeTextFile("./dest/index.html", htmlFile);
+  }
 }
